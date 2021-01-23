3e10e3cb6a83379907861de8873b9c000dd6040b
ByMykel <bymykel5@gmail.com>
2021-01-24 00:43:51 +0100

    Update UserInfoProfile.vue

--- resources/js/Components/UserInfoProfile.vue
+++ resources/js/Components/UserInfoProfile.vue
@@ -1,68 +1,71 @@
 <template>
     <div
-        class="flex flex-col items-center mx-auto h-auto justify-center pt-10 p-1 mt-2 mb-12 text-black border border-gray-500 w-full rounded-md"
+        class="flex flex-col justify-center items-center mx-auto p-3 pt-5 mt-2 mb-12 border border-gray-500 w-full rounded-md"
     >
-        <div class="mx-auto p-4 w-full">
-            <div class="flex justify-center flex-col sm:flex-row mb-3">
-                <div class="mr-5 flex flex-col items-center">
-                    <img
-                        class="w-24 h-24 rounded-full mx-2 container object-cover"
-                        :src="user.profile_photo_url"
-                        alt=""
-                    />
-                    <div v-if="!profile">
-                        <FollowButton :user="user" />
-                    </div>
-                </div>
-                <div>
-                    <div>{{ user.name }}</div>
-                    <div class="text-blue-400">@{{ user.username }}</div>
-                    <div class="flex" v-if="user.location">
-                        <Icons icon="location" />
-                        <span class="pl-1 truncate w-48" :title="user.location">
-                            {{ user.location }}
-                        </span>
-                    </div>
-                    <div class="flex" v-if="user.website">
-                        <Icons icon="link" />
-                        <a
-                            class="pl-1 text-blue-400 hover:underline truncate w-48"
-                            :href="user.website"
-                            target="_blank"
-                        >
-                            {{ user.website }}
-                        </a>
-                    </div>
-                    <div class="flex">
-                        <Icons icon="calendar" /><span class="pl-1"
-                            >Joined {{ user.created_at_human }}</span
-                        >
-                    </div>
-                </div>
+        <div class="flex justify-center flex-col mb-3 w-full">
+            <div class="mr-5 flex flex-col items-center">
+                <img
+                    class="w-32 h-32 rounded-full mx-2 container object-cover shadow"
+                    :src="user.profile_photo_url"
+                />
             </div>
-            <div class="container break-all" v-if="user.bio">
-                {{ user.bio }}
-            </div>
-            <div class="flex justify-around p-1 text-center w-full pt-10">
+            <div
+                class="flex justify-between w-full pb-2 border-b mb-2 border-gray-200"
+            >
                 <div>
-                    <div>Opinions</div>
-                    <div class="text-blue-400">
-                        {{ user.opinions_count }}
+                    <div class="font-bold">{{ user.name }}</div>
+                    <div class="font-bold text-blue-400">
+                        @{{ user.username }}
                     </div>
                 </div>
-                <inertia-link :href="route('user.following', user)">
-                    <div>Following</div>
-                    <div class="text-blue-400">
-                        {{ user.following_count }}
-                    </div>
-                </inertia-link>
-                <inertia-link :href="route('user.followers', user)">
-                    <div>Followers</div>
-                    <div class="text-blue-400">
-                        {{ user.followers_count }}
-                    </div>
+                <FollowButton v-if="!profile" :user="user" />
+                <inertia-link
+                    :href="route('profile.show')"
+                    v-else
+                    class="flex justify-center items-center w-24 h-8 rounded-full my-2 bg-none border-2 border-blue-400 hover:bg-blue-200"
+                >
+                    Edit profile
                 </inertia-link>
             </div>
+            <div>
+                <div class="break-all pb-2" v-if="user.bio">
+                    {{ user.bio }}
+                </div>
+                <div class="flex" v-if="user.location">
+                    <Icons icon="location" />
+                    <span class="pl-1 truncate" :title="user.location">
+                        {{ user.location }}
+                    </span>
+                </div>
+                <div class="flex" v-if="user.website">
+                    <Icons icon="link" />
+                    <a
+                        class="pl-1 text-blue-400 hover:underline truncate"
+                        :href="user.website"
+                        target="_blank"
+                    >
+                        {{ user.website }}
+                    </a>
+                </div>
+                <div class="flex">
+                    <Icons icon="calendar" />
+                    <span class="pl-1">Joined {{ user.created_at_human }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-around p-1 text-center w-full pt-5">
+            <inertia-link :href="route('user.following', user)">
+                <div>Following</div>
+                <div class="text-blue-400 font-bold">
+                    {{ user.following_count }}
+                </div>
+            </inertia-link>
+            <inertia-link :href="route('user.followers', user)">
+                <div>Followers</div>
+                <div class="text-blue-400 font-bold">
+                    {{ user.followers_count }}
+                </div>
+            </inertia-link>
         </div>
     </div>
 </template>
