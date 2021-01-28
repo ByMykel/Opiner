ba8b2e25323734044f34f00e828017e8ae56c58b
ByMykel <bymykel5@gmail.com>
2021-01-28 11:00:49 +0100

    Add progress indicator.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,13 @@ import Vue from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
 import PortalVue from 'portal-vue';
 import VueObserveVisibility from 'vue-observe-visibility'
+import { InertiaProgress } from '@inertiajs/progress'
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
 Vue.use(VueObserveVisibility)
+InertiaProgress.init()
 
 
 const app = document.getElementById('app');
