1f3f5c373307c680270eb30e5631fd3e320502ec
ByMykel <bymykel5@gmail.com>
2021-02-03 16:57:06 +0100

    Update follow button.

--- resources/js/Components/FollowButton.vue
+++ resources/js/Components/FollowButton.vue
@@ -1,24 +1,28 @@
 <template>
     <button
         @click="follow(user)"
-        class="w-24 h-8 rounded-full my-2"
+        class="w-8 h-8 rounded-full my-2 flex justify-center items-center"
         :class="[
             user.follow
-                ? 'hover:bg-red-400 bg-blue-400'
-                : 'bg-none border-2 border-blue-400 hover:bg-blue-200',
+                ? 'hover:bg-red-400 bg-green-400'
+                : 'bg-none border-2 border-black hover:bg-green-400',
         ]"
         @mouseover="hoverButton = true"
         @mouseleave="hoverButton = false"
     >
         <span v-if="user.follow">
-            {{ user.follow && !hoverButton ? "Following" : "Unfollow" }}
+            <Icons v-if="user.follow && !hoverButton" icon="check" />
+            <Icons v-else icon="x" />
         </span>
-        <span v-else>Follow</span>
+        <span v-else>
+            <Icons icon="plus" />
+        </span>
     </button>
 </template>
 
 <script>
 import { Inertia } from "@inertiajs/inertia";
+import Icons from "@/Components/Icons";
 
 export default {
     props: {
@@ -28,6 +32,9 @@ export default {
         return {
             hoverButton: false,
         };
+    },
+    components: {
+        Icons,
     },
     methods: {
         follow(user) {
--- resources/js/Components/Icons.vue
+++ resources/js/Components/Icons.vue
@@ -198,6 +198,54 @@
                 ></path>
             </svg>
         </div>
+        <div v-else-if="icon === 'check'">
+            <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                ></path>
+            </svg>
+        </div>
+        <div v-else-if="icon === 'plus'">
+            <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                ></path>
+            </svg>
+        </div>
+        <div v-else-if="icon === 'x'">
+            <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                ></path>
+            </svg>
+        </div>
     </div>
 </template>
 
