9bcef49f9dd5d86d12152a774ca06f822a772e23
ByMykel <bymykel5@gmail.com>
2021-02-03 16:24:21 +0100

    Change rounded in icons.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -3,31 +3,31 @@
         <div class="min-h-screen flex max-w-6xl mx-auto">
             <nav class="sm:pl-3 flex sm:block fixed p-1 sm:pt-20 bg-gray-200 sm:bg-transparent bottom-0 w-full sm:top-0 sm:w-min justify-around">
                 <inertia-link class="w-auto" :href="route('home')">
-                    <div class="flex rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('home') }">
+                    <div class="flex rounded-full md:rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('home') }">
                         <Icons icon="home" />
                         <span class="px-1 hidden md:block">Home</span>
                     </div>
                 </inertia-link>
                 <inertia-link :href="route('explore')">
-                    <div class="flex rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('explore') }">
+                    <div class="flex rounded-full md:rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('explore') }">
                         <Icons icon="search" />
                         <span class="px-1 hidden md:block">Explore</span>
                     </div>
                 </inertia-link>
                 <inertia-link :href="route('notifications')">
-                    <div class="flex rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('notifications') }">
+                    <div class="flex rounded-full md:rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('notifications') }">
                         <Icons icon="bell" :class="{'animate-bounce' : $page.props.notifications_count}" />
                         <span class="px-1 hidden md:block">Notifications</span>
                     </div>
                 </inertia-link>
                 <inertia-link :href="route('user', $page.props.auth)">
-                    <div class="flex rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('user', $page.props.auth) }">
+                    <div class="flex rounded-full md:rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('user', $page.props.auth) }">
                         <Icons icon="user" />
                         <span class="px-1 hidden md:block">Profile</span>
                     </div>
                 </inertia-link>
                 <inertia-link :href="route('profile.show')">
-                    <div class="flex rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('profile.show') }">
+                    <div class="flex rounded-full md:rounded-md p-1 my-1 hover:bg-gray-50" :class="{ 'bg-gray-50' : route().current('profile.show') }">
                         <Icons icon="cog" />
                         <span class="px-1 hidden md:block">Settings</span>
                     </div>
